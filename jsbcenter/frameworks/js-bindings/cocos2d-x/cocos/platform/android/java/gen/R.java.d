d024a5ac54335998a58dab0ebb4f81605fd5431a
DuHai bo <tim@DuHaideMac-mini.local>
2014-09-04 12:00:07 +0800

    add frameworks

---
+++ jsbcenter/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/tim/Desktop/devTools/cocos2d-js-v3.0-rc3/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
---
+++ jsbcenter/frameworks/js-bindings/cocos2d-x/cocos/scripting/lua-bindings/auto/api/AssetsManager.lua
@@ -0,0 +1,45 @@
+
+--------------------------------
+-- @module AssetsManager
+-- @extend Ref
+
+--------------------------------
+-- @function [parent=#AssetsManager] getState 
+-- @param self
+-- @return AssetsManager::State#AssetsManager::State ret (return value: cc.AssetsManager::State)
+        
+--------------------------------
+-- @function [parent=#AssetsManager] checkUpdate 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#AssetsManager] getStoragePath 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+--------------------------------
+-- @function [parent=#AssetsManager] update 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#AssetsManager] getLocalManifest 
+-- @param self
+-- @return Manifest#Manifest ret (return value: cc.Manifest)
+        
+--------------------------------
+-- @function [parent=#AssetsManager] getRemoteManifest 
+-- @param self
+-- @return Manifest#Manifest ret (return value: cc.Manifest)
+        
+--------------------------------
+-- @function [parent=#AssetsManager] downloadFailedAssets 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#AssetsManager] create 
+-- @param self
+-- @param #string str
+-- @param #string str
+-- @return AssetsManager#AssetsManager ret (return value: cc.AssetsManager)
+        
+return nil
